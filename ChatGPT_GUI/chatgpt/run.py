d7c94cfad1375782f7e1fcd21f6898c0d86bb4a2
AbdallahZein12 <94486924+AbdallahZein12@users.noreply.github.com>
2023-05-11 02:13:43 -0400

    Delete ChatGPT_GUI directory

--- ChatGPT_GUI/chatgpt/run.py
+++
@@ -1,8 +0,0 @@
-from chatgpt.chatgpt import ChatGpt
-
-def default_login(email,password):
-    with ChatGpt() as bot:
-        bot.land_first_page()
-        result = bot.default_login(email,password)
-        return result
-        
