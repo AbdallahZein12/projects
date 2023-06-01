fda2ff23b694ea6eb7b22c1202191dabc0e9753c
AbdallahZein12 <abdallahabdelmoneim7@gmail.com>
2023-06-01 11:48:59 -0400

    Added Folder for pic_to_ascii

---
+++ Pic_to_Ascii/test/tutorial6.py
@@ -0,0 +1,47 @@
+from flask import Flask, redirect, url_for, flash ,session , render_template, request
+from datetime import timedelta
+
+
+app = Flask(__name__)
+app.secret_key = "hello"
+app.permanent_session_lifetime = timedelta(days=1)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/login", methods=["POST","GET"])
+def login():
+    if request.method == "POST":
+        session.permanent = True
+        user = request.form["nm"]
+        session["user"] = user
+        flash(f"Login Successful")
+        return redirect(url_for("user"))
+    else:
+        if "user" in session:
+            flash("Already logged in!")
+            return redirect(url_for("user"))
+        return render_template("login.html")
+    
+    
+@app.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return render_template("user.html", user=user)
+    else:
+        flash("You are not logged in!")
+        return redirect(url_for("login"))
+    
+@app.route("/logout")
+def logout():
+    flash("You have been logged out", "info")
+    session.pop("user", None)
+    return redirect(url_for("login"))
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
+    
+    
