6dfd1a9cda1dc98c407644d536701c0232e9bf3b
AbdallahZein12 <abdallahabdelmoneim7@gmail.com>
2023-06-09 23:22:50 -0400

    Project PIC_TO_ASCII home page and backend operations are completed

--- Pic_to_Ascii/main.py
+++ Pic_to_Ascii/main.py
@@ -1,11 +1,66 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request
+import os
+from PIL import Image
+import json
+
+
 
 app = Flask(__name__)
 
-@app.route("/")
-@app.route("/home")
+@app.route("/",methods=["POST","GET"])
+@app.route("/home", methods=["GET","POST"])
 def home():
-    return render_template("home.html")
+    if request.method == "POST":
+        file = request.files['file']
+        dir = os.path.join("file_handling",file.filename)
+        file.save(dir)
+        with open("output.txt",'w') as output:
+            with Image.open(dir) as im:
+                im = im.convert('L')
+                im = im.resize((80,60))
+                chars = '.:-=+*#%@'
+                for y in range(im.size[1]):
+                    for x in range(im.size[0]):
+                        gray = im.getpixel((x,y))
+                        char = chars[gray * len(chars) // 355]
+                        output.write(char)
+                    output.write("\n")
+        with open("output.txt",'r') as output:    
+            final_output = output.read()
+        os.remove(dir)
+        with open("output.txt",'w') as output:
+            output.truncate(0)
+            
+        return json.dumps({"output": final_output})
+    
+    
+    return render_template("home.html",output="")
+
+# @app.route('/upload',methods=['POST'])
+# def upload_file():
+#     file = request.files['file']
+#     dir = os.path.join("file_handling",file.filename)
+#     file.save(dir)
+#     with open("output.txt",'w') as output:
+#         with Image.open(dir) as im:
+#             im = im.convert('L')
+#             im = im.resize((80,60))
+#             chars = '.:-=+*#%@'
+#             for y in range(im.size[1]):
+#                 for x in range(im.size[0]):
+#                     gray = im.getpixel((x,y))
+#                     char = chars[gray * len(chars) // 355]
+#                     output.write(char)
+#                 output.write("\n")
+#     with open("output.txt",'r') as output:    
+#         final_output = output.read()
+#     os.remove(dir)
+#     with open("output.txt",'w') as output:
+#         output.truncate(0)
+    
+#     return render_template("home.html",output=final_output)
+
+    
 
 
 if __name__ == "__main__":
--- Pic_to_Ascii/static/scripts/home/script.js
+++ Pic_to_Ascii/static/scripts/home/script.js
@@ -1,6 +1,69 @@
 function copyText() {
-    var textarea = document.getElementById("response");
-    textarea.select();
-    document.execCommand("copy");
-    alert("Copied Successfully!")
+  var textarea = document.getElementById("response");
+  textarea.select();
+  document.execCommand("copy")
+  alert("Copied Successfully!")
+}
+
+const dropZone = document.getElementById("drop-zone");
+const outputContainer = document.getElementById("response");
+const fileInput = document.getElementById("file-input")
+
+dropZone.addEventListener('dragover', (e) => {
+  e.preventDefault();
+  dropZone.classList.add('highlight');
+});
+
+dropZone.addEventListener('dragleave', () => {
+  dropZone.classList.remove('highlight');
+});
+
+dropZone.addEventListener('drop', (e) => {
+  e.preventDefault();
+  dropZone.classList.remove('highlight');
+
+  const file = e.dataTransfer.files[0];
+
+  // Check if the dropped file is in PNG or JPG format
+  if (file && (file.type === 'image/png' || file.type === 'image/jpeg')) {
+    uploadFile(file);
+  } else {
+    alert('Invalid file format');
   }
+
+});
+
+dropZone.addEventListener('click',() => {
+  fileInput.click()
+});
+
+fileInput.addEventListener('change', (e) => {
+  const file = e.target.files[0];
+  if (file && (file.type === 'image/png' || file.type === 'image/jpeg')) {
+    uploadFile(file);
+  } else {
+    alert("Invalid file format");
+  }
+})
+
+function uploadFile(file) {
+  const formData = new FormData();
+    formData.append('file', file);
+
+    // Send the file to the Flask backend using an AJAX request
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', '/home', true);
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        // File upload success
+        const response = JSON.parse(xhr.responseText);
+        outputContainer.textContent = response.output;
+        alert('File uploaded successfully');
+      } else {
+        // File upload failed
+        alert('Error uploading file');
+      }
+    };
+    xhr.send(formData);
+}
+
