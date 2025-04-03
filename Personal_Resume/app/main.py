3a1892dd97cf63639655909966662e2c33c9a78e
AbdallahZein12 <abdallahabdelmoneim7@gmail.com>
2025-04-03 00:31:20 -0400

    Added personal resume project files

--- Personal_Resume/app/main.py
+++ Personal_Resume/app/main.py
@@ -1,13 +1,40 @@
-from flask import Flask, render_template, url_for,redirect,request
+from flask import Flask, redirect, url_for, render_template, request
+import os
+from email.message import EmailMessage
+import ssl
+import smtplib
 
 
 app = Flask(__name__)
 
-@app.route("/",methods=["GET"])
-@app.route("/home",methods=["GET"])
-def home():
-    return render_template("home.html",output="")
 
-@app.route("/more")
-def more():
-    return render_template("more.html")
+@app.route("/", methods=["GET"])
+@app.route("/home", methods=[ "GET"])
+def home():
+  return render_template("index.html")
+
+
+@app.route("/about", methods=["GET"])
+def about():
+  return render_template("about.html")
+
+
+@app.route("/calendar", methods=["GET"])
+def calendar():
+  return render_template("calendar.html")
+
+
+@app.route("/contributions", methods=["GET"])
+def contributions():
+  return render_template("contributions.html")
+
+
+@app.route("/resources", methods=["GET"])
+def resources():
+  return render_template("resources.html")
+
+@app.route("/underconstruction")
+def underconstruction():
+  return "<img src='https://kidsarefrompluto.files.wordpress.com/2012/08/fr.jpg?w=584' style='position:absolute; top:0; bottom:0; right:0; left:0; margin:auto;'>"
+
+app.run(debug=True)
